85923bea485bff647dcf285e1be68587c6170df6
PapelRasgado <joaodantas31.jpd@gmail.com>
2018-11-26 08:44:09 -0300

    piru

--- CRUD/src/main/java/bean/UsuarioBean.java
+++ CRUD/src/main/java/bean/UsuarioBean.java
@@ -1,11 +1,20 @@
 package bean;
+import java.io.IOException;
 import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
 import java.util.Collection;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import entity.Usuario;
 import service.UsuService;
@@ -18,6 +27,8 @@ public class UsuarioBean implements Serializable {
 	private UsuService service;
 
 	protected Usuario entidade;
+	
+	protected String senha;
 
 	protected Collection<Usuario> entidades;
 
@@ -33,6 +44,32 @@ public class UsuarioBean implements Serializable {
 	public void remove(Usuario entidade) {
 		getService().remove(entidade);
 		limpar();
+	}
+	
+	public String getUserLogin() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		Principal userPrincipal = externalContext.getUserPrincipal();
+		if (userPrincipal == null) {
+			return "";
+		}
+		return userPrincipal.getName();
+	}
+	
+	public void efetuarLogout() throws IOException, ServletException {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		ExternalContext ec = fc.getExternalContext();
+		HttpSession session = (HttpSession) ec.getSession(false);
+		session.invalidate();
+		HttpServletRequest request = (HttpServletRequest) ec.getRequest();
+		request.logout();
+		ec.redirect(ec.getApplicationContextPath());
+	}
+	
+	public boolean isUserInRole(String role) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		return externalContext.isUserInRole(role);
 	}
 
 	public Usuario getEntidade() {
@@ -52,6 +89,14 @@ public class UsuarioBean implements Serializable {
 	}
 
 	public void save() {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(senha.getBytes());
+			entidade.setSenha(md.digest());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	
 		getService().save(entidade);
 		limpar();
 	}
@@ -72,6 +117,14 @@ public class UsuarioBean implements Serializable {
 
 	public UsuService getService() {
 		return service;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 }
